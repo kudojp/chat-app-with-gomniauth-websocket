ef0960c8c5739ff66bb29d0346aaa0e972db9e45
Daiki Kudo <heyjudejudejude1968@gmail.com>
2020-02-05 00:57:30 +0900

    Update README.md

--- client.go
+++ client.go
@@ -13,13 +13,13 @@ type client struct {
 	room   *chatroom       // 所属するチャットルーム
 }
 
-// websocketに書き込まれたメッセージを読み込みroomへ送る
+// websocketに書き込まれたメッセージを読み込みroomのforwardチャネルへ送る
 func (c *client) read() {
-	// webソケットからjson形式でメッセージを読み出し、
-	// forwardチャネルに流す
-	//　読み込みは無限ループで実行される
+	// webソケットからjson形式でメッセージを読み出し、forwardチャネルに流す
+	// 読み込みは無限ループで実行される
 	for {
 		var msg *message
+		// クライアントからWSでメッセージを受け取る
 		if err := c.socket.ReadJSON(&msg); err == nil {
 			t := time.Now()
 			layout := "2006-01-02 15:04:05"
@@ -29,15 +29,16 @@ func (c *client) read() {
 			break
 		}
 	}
-	c.socket.Close()
+	// c.socket.Close()
 }
 
 // roomから送られたメッセージを読み込みwebsocketに書き込む
 func (c *client) write() {
+	// チャネルが閉じるまでの実質無限ループ
 	for msg := range c.send {
+		// チャネル内のメッセージをクライアントへ送る
 		if err := c.socket.WriteJSON(msg); err != nil {
 			break
 		}
-		c.socket.Close()
 	}
 }
